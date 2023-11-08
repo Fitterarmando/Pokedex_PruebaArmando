0252cf11140a7136b258e2d2d80ea5c2a65679fc
Armando Frías <armando_ramirez097@hotmail.com>
2023-11-08 11:48:19 -0600

    Change name of home to pokemon

--- Pokedex_PruebaArmando/Domain/PokemonDBModel.swift
+++ Pokedex_PruebaArmando/Domain/PokemonDBModel.swift
@@ -1,16 +1,8 @@
 //
-//  HomeVC.swift
+//  PokemonDBModel.swift
 //  Pokedex_PruebaArmando
 //
 //  Created by Juan Armando Frías Ramírez on 08/11/23.
 //
 
 import Foundation
-import UIKit
-
-
-class HomeVC : UIViewController {
-    
-    
-    
-}
---
+++ Pokedex_PruebaArmando/Domain/PokemonRepository.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonRepository.swift
+//  Pokedex_PruebaArmando
+//
+//  Created by Juan Armando Frías Ramírez on 08/11/23.
+//
+
+import Foundation
